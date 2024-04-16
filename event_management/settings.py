a55e69cc538d9c1b48b20d6653c62542e45ce212
Philip Odey <philipodey75@gmail.com>
2024-04-16 01:30:00 +0100

    dtabase commit

--- event_management/settings.py
+++ event_management/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PASSWORD': 'Happinex@75',
     }
 }
 
